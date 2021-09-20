022178a48c9821e57cb625b635ff5ee5df8889ec
alumah00 <chrishamula@gmail.com>
2021-09-20 16:34:24 -0400

    Updates complete but no bonus

--- scrabble_scorer.py
+++ scrabble_scorer.py
@@ -13,14 +13,14 @@ OLD_POINT_STRUCTURE = {
 
 def old_scrabble_scorer(word):
     word = word.upper()
-    letterPoints = ""
+    letterPoints = 0
 
     for char in word:
 
-        for point_value in old_point_structure:
+        for point_value in OLD_POINT_STRUCTURE:
 
-            if char in old_point_structure[point_value]:
-                letterPoints += 'Points for {char}: {point_value}\n'.format(char = char, point_value = point_value)
+            if char in OLD_POINT_STRUCTURE[point_value]:
+                letterPoints += point_value
 
     return letterPoints
 
@@ -28,25 +28,98 @@ def old_scrabble_scorer(word):
 # don't change the names or your program won't work as expected.
 
 def initial_prompt():
-   print("Let's play some Scrabble!\n")
+   choice = input("Let's play some Scrabble!\n\nEnter a word to score:")
+   return choice
 
+def simple_scorer(word):
+    word = word.lower()
+    points = 0
+    for char in word:
+        points += 1
+    return points
 
-def simple_scorer():
-   return 
+def vowel_bonus_scorer(word):
+    word = word.lower()
+    points = 0
+    for char in word:
+        if char in ["a", "e", "i", "o", "u"]:
+            points += 3
+        else: points += 1
+    return points 
 
-def vowel_bonus_scorer():
-    return 
-
-def scrabble_scorer():
-    return
+def scrabble_scorer(word):
+    word = word.lower()
+    points = 0
+    for letter in word:
+        if letter in new_point_structure:
+            points += new_point_structure[letter]
+    return points
 
 scoring_algorithms = ()
 
 def scorer_prompt():
-    return 
+    print('Which scoring algorithm would you like to use?')
+    
+    for index, algorithm in enumerate(scoring_algorithms):
+        print(f'{index} - {algorithm["name"]}: {algorithm["description"]}')
 
-def transform():
-    return
+    selection = int(input('Enter 0, 1, or 2:'))
+
+    selected_score_algorithm_dict = scoring_algorithms[selection]
+
+    return selected_score_algorithm_dict
+
+
+def transform(provided_dict):
+    new_dict = {}
+
+    new_dict["' '"] = 0
+
+    for (key, value) in provided_dict.items():
+        for letter in value:
+            new_dict[letter.lower()] = key
+
+    return new_dict
+
+
+new_point_structure = transform(OLD_POINT_STRUCTURE)
+
+simple_scorer_dict = {
+    'name': 'Simple',
+    'description': 'scores each letter provided is worth 1 point.',
+    'score_function': simple_scorer
+}
+
+vowel_bonus_scorer_dict = {
+    'name': 'Bonus Vowels',
+    'description': 'gives 3 point per vowels and 1 point per consonants',
+    'score_function': vowel_bonus_scorer
+}
+
+old_scrabble_scorer_dict = {
+    'name': 'Scrabble',
+    'description': 'uses the original Scrabble game point system',
+    'score_function': scrabble_scorer
+}
+
+scoring_algorithms = (
+    simple_scorer_dict,
+    vowel_bonus_scorer_dict,
+    old_scrabble_scorer_dict
+)
+
 
 def run_program():
     word = initial_prompt()
+
+    selected_score_algorithm_dict = scorer_prompt()
+
+    points = selected_score_algorithm_dict['score_function'](word)
+
+    print(
+        f'''
+The word you entered was "{word}".
+You selected the "{selected_score_algorithm_dict["name"]}" scoring algorithm which {selected_score_algorithm_dict["description"]}.
+Your word is worth {points} points!'''
+    )
+
